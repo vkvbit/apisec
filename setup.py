36dfa73aa5ef768ef0ae2486529fb5a2ce5b1005
vkvbit <myselfv@hotmail.com>
2024-10-11 17:33:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,40 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
 
 setup(
     name='apisec',
-    version='0.1',
-    packages=find_packages(),
+    version='1.0',
+    packages=['apisec'],
+    url='https://github.com/vkvbit/apisec',
+    download_url='https://github.com/vkvbit/apisec',
+    license='MIT',
+    author='Vaibhav Kumar',
+    author_email='myselfv@hotmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['apisec', 'rest', 'graphql', 'soap', 'restful, api', 'security', 'scanner', 'tool', "vulnerability"],    
     entry_points={
         'console_scripts': [
             'apisec = apisec.__main__:main'
-        ]
+         ]
     },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     install_requires=[
-        "offat"
+        'offat',
+        'argparse',
+        'os',
+        'sys'
     ],
 )
